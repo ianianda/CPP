5116ed7d0ab8e734d645584403bb499b70cab2a4
ianianda <ywang402@my.bcit.ca>
2017-09-28 08:57:53 -0700

    no message

--- Labs/Lab3/Main.cpp
+++ Labs/Lab3/Main.cpp
@@ -1,6 +1,12 @@
+#include "TimeSheet.h"
 #include <iostream>
 using namespace std;
 
-int main()
+int main(const char* name, const int *day)
 {
+	lab3::AddTime(std::cin, std::cout); // time worked each day. hours < 10, otherwise, ignore.
+	lab3::GetTotalTime(std::cin, std::cout);  // time worked totally. hours + hours + hours...
+	lab3::GetAverageTime(std::cin, std::cout); // total hours / total days
+	std::string& GetName(std::cin, std::cout); //name of employee.
+	return 0;
 }
