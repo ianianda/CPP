865650fb4f4a22b40f0c735552ad3575c40145ae
ianianda <ywang402@my.bcit.ca>
2017-11-01 14:20:30 -0700

    no message

--- Assignments/Assignment2/Assignment2.h
+++ Assignments/Assignment2/Assignment2.h
@@ -46,93 +46,93 @@ namespace assignment2
 		return mapInserted;
 	}
 
-	template <typename K, class V>
-	std::vector<K> GetKeys(const std::map<K, V>& m)
-	{
-		std::vector<K> keys;
-		MapIterator<K, V> it;
-		//typename std::vector<K>::iterator itFound;
+	//template <typename K, class V>
+	//std::vector<K> GetKeys(const std::map<K, V>& m)
+	//{
+	//	std::vector<K> keys;
+	//	MapIterator<K, V> it;
+	//	//typename std::vector<K>::iterator itFound;
 
-		for (it = m.begin(); it != m.end(); it++)
-		{
-			keys.push_back(it->first);
-		}
+	//	for (it = m.begin(); it != m.end(); it++)
+	//	{
+	//		keys.push_back(it->first);
+	//	}
 
-		return keys;
-	}
+	//	return keys;
+	//}
 
-	template <typename K, class V>
-	std::vector<V> GetValues(const std::map<K, V>& map)
-	{
-		MapIterator<K, V> it;
-		std::vector<V> result;
+	//template <typename K, class V>
+	//std::vector<V> GetValues(const std::map<K, V>& map)
+	//{
+	//	MapIterator<K, V> it;
+	//	std::vector<V> result;
 
-		for (it = map.begin(); it != map.end(); ++it)
-		{
-			result.push_back(it->second);
-		}
-		return result;
-	}
+	//	for (it = map.begin(); it != map.end(); ++it)
+	//	{
+	//		result.push_back(it->second);
+	//	}
+	//	return result;
+	//}
 
-	template <typename T>
-	std::vector<T> Reverse(const std::vector<T>& v)
-	{
-		std::vector<T> copyV;
-		copyV.assign(v.begin(), v.end());
-		std::reverse(copyV.begin(), copyV.end());
-		return copyV;
-	}
+	//template <typename T>
+	//std::vector<T> Reverse(const std::vector<T>& v)
+	//{
+	//	std::vector<T> copyV;
+	//	copyV.assign(v.begin(), v.end());
+	//	std::reverse(copyV.begin(), copyV.end());
+	//	return copyV;
+	//}
 
-	template <typename T>
-	std::vector<T> operator+(const std::vector<T>& v1, const std::vector<T>& v2)
-	{
-		std::vector<T> newVector;
-		for (int i = 0; i < v1.size(); i++) {
-			newVector.push_back(v1.at[i]);
-		}
-		newVector.insert(newVector.end(), v2.begin(), v2.end());
+	//template <typename T>
+	//std::vector<T> operator+(const std::vector<T>& v1, const std::vector<T>& v2)
+	//{
+	//	std::vector<T> newVector;
+	//	for (int i = 0; i < v1.size(); i++) {
+	//		newVector.push_back(v1.at[i]);
+	//	}
+	//	newVector.insert(newVector.end(), v2.begin(), v2.end());
 
-		return newVector;
-	}
+	//	return newVector;
+	//}
 
-	template <typename K, class V>
-	std::map<K, V> operator+(const std::map<K, V>& m1, const std::map<K, V>& m2)
-	{
-		std::map<K, V> newMap;
-		newMap.insert(m1.begin(), m1.end());
+	//template <typename K, class V>
+	//std::map<K, V> operator+(const std::map<K, V>& m1, const std::map<K, V>& m2)
+	//{
+	//	std::map<K, V> newMap;
+	//	newMap.insert(m1.begin(), m1.end());
 
-		for (int i = 0; i < m2.size(); i++)
-		{
-			for (int j = 0; j < newMap.size(); j++)
-			{
-				if (!newMap.find(m2->first))
-				{
-					newMap.insert(m2.at(i));
-				}
-			}
-		}
-		return newMap;
-	}
+	//	for (int i = 0; i < m2.size(); i++)
+	//	{
+	//		for (int j = 0; j < newMap.size(); j++)
+	//		{
+	//			if (!newMap.find(m2->first))
+	//			{
+	//				newMap.insert(m2.at(i));
+	//			}
+	//		}
+	//	}
+	//	return newMap;
+	//}
 
-	template<typename t>
-	std::ostream& operator<<(std::ostream& os, const std::vector<t>& v)
-	{
-		for (int i = 0; i < v.size(); i++)
-		{
-			os << v.at(i) << ", ";
-		}
-		return os; //return a reference to the ostream object
-	}
+	//template<typename t>
+	//std::ostream& operator<<(std::ostream& os, const std::vector<t>& v)
+	//{
+	//	for (int i = 0; i < v.size(); i++)
+	//	{
+	//		os << v.at(i) << ", ";
+	//	}
+	//	return os; //return a reference to the ostream object
+	//}
 
-	template <typename K, class V>
-	std::ostream& operator<<(std::ostream& os, const std::map<K, V>& m)
-	{
-		for (const auto& p : m)
-		{
-			os << "{ " << p.first << ", ";
-			//for(int x: p.second) os<<x<<'';
-			os << p.second << " }";
-			os << std::endl;
-		}
-	}
+	//template <typename K, class V>
+	//std::ostream& operator<<(std::ostream& os, const std::map<K, V>& m)
+	//{
+	//	for (const auto& p : m)
+	//	{
+	//		os << "{ " << p.first << ", ";
+	//		//for(int x: p.second) os<<x<<'';
+	//		os << p.second << " }";
+	//		os << std::endl;
+	//	}
+	//}
 }
