fa39699d7b3498f56851ceee8b045c9216e5b7ba
ianianda <ywang402@my.bcit.ca>
2017-11-01 14:14:04 -0700

    no message

--- Assignments/Assignment2/Assignment2.h
+++ Assignments/Assignment2/Assignment2.h
@@ -4,55 +4,135 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <list>
+#include <iterator>
+#include <string>
+
 
 namespace assignment2
 {
 	template <typename K, class V>
+	using MapIterator = typename std::map<K, V>::const_iterator;
+
+	template <typename K, class V>
 	std::map<K, V> ConvertVectorsToMap(const std::vector<K>& keys, const std::vector<V>& data)
 	{
-		// implement ConvertVectorsToMap here
+		std::map<K, V> mapInserted;
+		int length = 0;
+		if (keys.size() < data.size())
+		{
+			length = keys.size();
+		}
+		else
+		{
+			length = data.size();
+		}
+
+		for (int i = 0; i < length; i++)
+		{
+			if (i == 0)
+			{
+				mapInserted.insert(std::make_pair(keys.at(i), data.at(i)));
+			}
+
+			for (int j = 0; j < mapInserted.size(); j++)
+			{
+				if (mapInserted.find(keys.at(i)) == mapInserted.end())
+				{
+					mapInserted.insert(std::make_pair(keys.at(i), data.at(i)));
+				}
+			}
+		}
+		return mapInserted;
 	}
 
 	template <typename K, class V>
 	std::vector<K> GetKeys(const std::map<K, V>& m)
 	{
-		// implement GetKeys here		
+		std::vector<K> keys;
+		MapIterator<K, V> it;
+		//typename std::vector<K>::iterator itFound;
+
+		for (it = m.begin(); it != m.end(); it++)
+		{
+			keys.push_back(it->first);
+		}
+
+		return keys;
 	}
 
 	template <typename K, class V>
 	std::vector<V> GetValues(const std::map<K, V>& map)
 	{
-		// implement GetValues here
+		MapIterator<K, V> it;
+		std::vector<V> result;
+
+		for (it = map.begin(); it != map.end(); ++it)
+		{
+			result.push_back(it->second);
+		}
+		return result;
 	}
 
 	template <typename T>
 	std::vector<T> Reverse(const std::vector<T>& v)
 	{
-		// implement Reverse here
+		std::vector<T> copyV;
+		copyV.assign(v.begin(), v.end());
+		std::reverse(copyV.begin(), copyV.end());
+		return copyV;
 	}
 
-}
+	template <typename T>
+	std::vector<T> operator+(const std::vector<T>& v1, const std::vector<T>& v2)
+	{
+		std::vector<T> newVector;
+		for (int i = 0; i < v1.size(); i++) {
+			newVector.push_back(v1.at[i]);
+		}
+		newVector.insert(newVector.end(), v2.begin(), v2.end());
 
-template <typename T>
-std::vector<T> operator+(const std::vector<T>& v1, const std::vector<T>& v2)
-{
-	// implement operator '+' here
-}
+		return newVector;
+	}
 
-template <typename K, class V>
-std::map<K, V> operator+(const std::map<K, V>& m1, const std::map<K, V>& m2)
-{
-	// implement operator '+' here
-}
+	template <typename K, class V>
+	std::map<K, V> operator+(const std::map<K, V>& m1, const std::map<K, V>& m2)
+	{
+		std::map<K, V> newMap;
+		newMap.insert(m1.begin(), m1.end());
 
-template<typename T>
-std::ostream& operator<<(std::ostream& os, const std::vector<T>& v)
-{
-	// implement operator '<<' here
-}
+		for (int i = 0; i < m2.size(); i++)
+		{
+			for (int j = 0; j < newMap.size(); j++)
+			{
+				if (!newMap.find(m2->first))
+				{
+					newMap.insert(m2.at(i));
+				}
+			}
+		}
+		return newMap;
+	}
 
-template <typename K, class V>
-std::ostream& operator<<(std::ostream& os, const std::map<K, V>& m)
-{
-	// implement operator '<<' here
+	template<typename t>
+	std::ostream& operator<<(std::ostream& os, const std::vector<t>& v)
+	{
+		for (int i = 0; i < v.size(); i++)
+		{
+			os << v.at(i) << ", ";
+		}
+		return os; //return a reference to the ostream object
+	}
+
+	template <typename K, class V>
+	std::ostream& operator<<(std::ostream& os, const std::map<K, V>& m)
+	{
+		for (const auto& p : m)
+		{
+			os << "{ " << p.first << ", ";
+			//for(int x: p.second) os<<x<<'';
+			os << p.second << " }";
+			os << std::endl;
+		}
+	}
 }
---
+++ Assignments/Assignment2/main.cpp
@@ -0,0 +1,48 @@
+#include "Assignment2.h"
+#include <iostream>
+#include <string>
+
+int main() {
+	std::vector<int> v1;
+	v1.push_back(1);
+	v1.push_back(2);
+	v1.push_back(3);
+	v1.push_back(4);
+
+	std::vector<char> v2;
+	v2.push_back('a');
+	v2.push_back('b');
+	v2.push_back('c');
+	v2.push_back('D');
+
+	std::vector<int> v3;
+	v3.push_back(4);
+	v3.push_back(5);
+	v3.push_back(6);
+
+	std::map<char, int> m1;
+	m1['a'] = 1;
+	m1['b'] = 2;
+	m1['c'] = 3;
+
+	std::map<char, int> m2;
+	m2['d'] = 4;
+	m2['e'] = 5;
+	m2['f'] = 6;
+
+	//std::map<int, char> m = assignment2::ConvertVectorsToMap(v1, v2);
+	//std::vector<char> keys = assignment2::GetKeys(m1);
+	//std::vector<int> values = assignment2::GetValues(m1);
+	//std::vector<int> reversedV1 = assignment2::Reverse(v1);
+    
+	//std::vector<int> combinedVector = v1 + v3;
+	//std::vector<int> ioperator = v1 + v3;
+
+	//std::cout << v1 << std::endl;
+	//std::cout << m1 << std::endl;
+	
+	//std::map<int, int> m = {1,2};
+
+	//std::cout << m << std::endl;
+	return 0;
+}
