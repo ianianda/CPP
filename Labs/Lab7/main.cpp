8db75b2143797fa9fbe903c82a8ef239d6a129db
ianianda <ywang402@my.bcit.ca>
2017-12-01 12:25:34 -0800

    no message

--- Labs/Lab7/main.cpp
+++ Labs/Lab7/main.cpp
@@ -10,12 +10,9 @@ int main()
 	list.Insert(std::make_unique<float>(1.0f));
 	list.Insert(std::make_unique<float>(2.0f));
 	list.Insert(std::make_unique<float>(3.0f));
-
 	list.Insert(std::make_unique<float>(4.0f), 1);
 	list.Insert(std::make_unique<float>(5.0f), 3);
-	
 	bool bExists = list.Search(0);
 	unsigned int size = list.GetLength();
-	
 	return 0;
 }
