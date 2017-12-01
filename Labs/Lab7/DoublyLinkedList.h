4403d68a9a6ef6cbe4ac6179c9dba72c703ded82
ianianda <ywang402@my.bcit.ca>
2017-11-30 18:59:30 -0800

    no message

--- Labs/Lab7/DoublyLinkedList.h
+++ Labs/Lab7/DoublyLinkedList.h
@@ -36,11 +36,10 @@ namespace lab7
 	void DoublyLinkedList<T>::Insert(std::unique_ptr<T> data)
 	{
 		std::shared_ptr<Node<T>> node_ptr = mRoot;
-		
+		//std::shared_ptr<Node<T>> temp;
 		if (mRoot == nullptr)
 		{
-			node_ptr = std::make_shared<Node<T>>(std::move(data));
-			mRoot = node_ptr;
+			mRoot = std::make_shared<Node<T>>(std::move(data));
 			length++;
 		}
 		else //mRoot != nullptr
@@ -51,8 +50,8 @@ namespace lab7
 			}
 			while (node_ptr->Next != nullptr);
 
-			node_ptr = std::make_shared<Node<T>>(std::move(data));
-			mRoot = node_ptr;
+			node_ptr->Next = std::make_shared<Node<T>>(std::move(data));
+			node_ptr->Next->Previous = node_ptr;
 			length++;
 		}
 	}
@@ -95,15 +94,13 @@ namespace lab7
 				}
 				while (i < index);
 				
+				
 				temp2 = std::make_shared<Node<T>>(std::move(data));
-				temp2->Next = temp;
-				temp->Previous = temp2;
-
 				temp3 = temp->Previous.lock();
 				temp3->Next = temp2;
 				temp2->Previous = temp3;
-
-				mRoot = temp3;
+				temp2->Next = temp;
+				temp->Previous = temp2;
 				length++;
 			}
 		}
