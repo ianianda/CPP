823610c65168e8823ac8601b0d5a5bc226fdc21c
DESKTOP-ROB5TAF\kingt <kingtaem@hotmail.com>
2017-12-07 09:19:55 -0800

    no message

--- Labs/Lab8/main.cpp
+++ Labs/Lab8/main.cpp
@@ -7,12 +7,10 @@ int main()
 	lab8::Storage<int> storage1(10000);
 	lab8::Storage<int> storage1Copy(std::move(storage1)); // storage1's memory should be nulled/zeroed
 
-														  // using assignment operator
 	lab8::Storage<char> storage2(20000);
 	lab8::Storage<char> storage2Copy(3);
 	storage2Copy = std::move(storage2); // storage2's memory should be nulled/zeroed
 
-										// using regular copy constructor
 	lab8::Storage<double> storage3(10000000);
 	lab8::Storage<double> storage3Copy(storage3); // storage3 still has its own data
 
